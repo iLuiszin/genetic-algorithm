8c9d1b3a44738985bbf44ef6bf4f272ed79adc1a
Gustavo P <gpedr56@gmail.com>
2024-03-20 08:13:41 -0300

    fix

--- index.js
+++ index.js
@@ -1,148 +1,138 @@
-//Atividade 1
+//Gerando população
 const cromLimite = [
-  [14, 21],
-  [1, 4],
-  [0, 2],
-  [0, 23],
+    [14, 21],
+    [1, 4],
+    [0, 2],
+    [0, 23],
 ]
 const nInd = 5
 const nCrom = 4
 
 let individual = []
 let population = []
+
 function novaPopulacao(nInd, cromLimite) {
-  let nCromossomos = cromLimite.length
+    let nCromossomos = cromLimite.length
 
-  for (let j = 0; j < nInd; j++) {
-    for (let i = 0; i < nCromossomos; i++) {
-      let inf = cromLimite[i][0]
-      let sup = cromLimite[i][1]
-      individual.push(Math.floor(Math.random() * (sup - inf + 1) + inf))
+    for (let j = 0; j < nInd; j++) {
+        for (let i = 0; i < nCromossomos; i++) {
+            let inf = cromLimite[i][0]
+            let sup = cromLimite[i][1]
+            individual.push(Math.floor(Math.random() * (sup - inf + 1) + inf))
+        }
+        population.push(individual)
+        individual = []
     }
-    population.push(individual)
-    individual = []
-  }
 
-  return population
+    return population
 }
 
+function catchValuesArray(populacao) {
+    const numLinhas = populacao.length
+    const numColunas = populacao[0].length
 
-//Atividade 2
-// Primeira Parte
-function convertToBin(population) {
-  let fullString = ''
-  let bits = []
-  let decimalNumbers = []
-  const populationFinal = []
-  for (let i = 0; i < population[0].length; i++) {
-    for (let j = 0; j < population.length; j++) {
-      let value = population[j][i]
-      const valueInDecimal = value.toString(2)
-      decimalNumbers.push(valueInDecimal)
-      bits.push(valueInDecimal.length)
+    const novaPopulacao = []
+
+    for (let i = 0; i < numColunas; i++) {
+        novaPopulacao.push([])
     }
 
-    const maxLengthOfBits = Math.max(...bits)
-    for (let k = 0; k < population.length; k++) {
-      let value = decimalNumbers[k]
-      let zeros = maxLengthOfBits - value.length
-      let string = ''
-      for (let l = 0; l < zeros; l++) {
-        string += '0'
-      }
-      string += value
-      fullString += string
+    for (let i = 0; i < numLinhas; i++) {
+        for (let j = 0; j < numColunas; j++) {
+            novaPopulacao[j].push(populacao[i][j])
+        }
     }
-    populationFinal.push({ binary: fullString, maxBits: maxLengthOfBits })
 
-    bits = []
-    decimalNumbers = []
-    fullString = ''
-  }
-
-  return populationFinal
+    return novaPopulacao
 }
 
-// Segunda Parte
-function decodeBinaries(populationFinal) {
-  let result = []
-  const nCromossomos = populationFinal.length
-  const nIndividuos =
-    populationFinal[0].binary.length / populationFinal[0].maxBits
-  for (let i = 0; i < nIndividuos; i++) {
-    let decoded = []
-    for (let j = 0; j < nCromossomos; j++) {
-      let binary = populationFinal[j].binary
-      let maxBits = populationFinal[j].maxBits
-      let chunk = binary.slice(i * maxBits, (i + 1) * maxBits)
-      let decimal = parseInt(chunk, 2)
-      decoded.push(decimal)
+function normalize(population) {
+    const minsAndMaxs = [];
+
+    catchValuesArray(population).forEach((valuesForNormalize) => {
+        const [min, max] = normalizeAux(valuesForNormalize)
+        minsAndMaxs.push([min, max])
+    })
+
+    population.forEach((individual) => {
+        for (let i = 0; i < individual.length; i++) {
+            individual[i] = Number(((individual[i] - minsAndMaxs[i][0]) / (minsAndMaxs[i][1] - minsAndMaxs[i][0])).toFixed(2))
+        }
+    })
+
+    return population
+
+}
+
+function normalizeAux(valuesForNormalize) {
+    const min = Math.min(...valuesForNormalize)
+    const max = Math.max(...valuesForNormalize)
+
+    return [min, max]
+}
+
+function fXFunction(characteristic, avg) {
+    let sum = 0
+    for (let i = 0; i < characteristic.length; i++) {
+        sum += Math.abs(characteristic[i] - avg[i])
     }
-    result.push(decoded)
-  }
-  return result
+    return sum
 }
 
 //Atividade 3
 // Primeira Parte
-function fitness(populacaoOrdenada) {
-  const newPopulation = []
-
-  for (let i = 0; i < populacaoOrdenada.length; i++) {
-    newPopulation.push({
-      populacao: populacaoOrdenada[i],
-      aptidao: funcaoObjetiva(populacaoOrdenada[i]),
-    })
-  }
-
-  return newPopulation
-}
-
-function funcaoObjetiva(x) {
-  const Ncrom = x.length
-  let objFunction = 10
-
-  for (let j = 0; j < Ncrom; j++) {
-    const value = x[j]
-    objFunction = objFunction - value * value
-  }
-
-  return objFunction
-}
-
-//Segunda Parte
-function metodoDaRoleta(populacao) {
-  let somaAptidao = 0
-  for (let i = 0; i < populacao.length; i++) {
-    somaAptidao += populacao[i].aptidao
-  }
-
-  for (let i = 0; i < populacao.length; i++) {
-    populacao[i].probabilidade = populacao[i].aptidao / somaAptidao
-  }
-
-  const faixasProbabilidade = []
-  let faixaAcumulada = 0
-  for (let i = 0; i < populacao.length; i++) {
-    faixaAcumulada += populacao[i].probabilidade
-    faixasProbabilidade.push(faixaAcumulada)
-  }
-
-  const sorteio = Math.random()
-  for (let i = 0; i < faixasProbabilidade.length; i++) {
-    if (sorteio <= faixasProbabilidade[i]) {
-      return populacao[i]
-    }
-  }
-}
+//function fitness(populacaoOrdenada) {
+//  const newPopulation = []
+//
+//  for (let i = 0; i < populacaoOrdenada.length; i++) {
+//    newPopulation.push({
+//      populacao: populacaoOrdenada[i],
+//      aptidao: funcaoObjetiva(populacaoOrdenada[i]),
+//    })
+//  }
+//
+//  return newPopulation
+//}
+//
+//function funcaoObjetiva(x) {
+//  const Ncrom = x.length
+//  let objFunction = 10
+//
+//  for (let j = 0; j < Ncrom; j++) {
+//    const value = x[j]
+//    objFunction = objFunction - value * value
+//  }
+//
+//  return objFunction
+//}
+//
+////Segunda Parte
+//function metodoDaRoleta(populacao) {
+//  let somaAptidao = 0
+//  for (let i = 0; i < populacao.length; i++) {
+//    somaAptidao += populacao[i].aptidao
+//  }
+//
+//  for (let i = 0; i < populacao.length; i++) {
+//    populacao[i].probabilidade = populacao[i].aptidao / somaAptidao
+//  }
+//
+//  const faixasProbabilidade = []
+//  let faixaAcumulada = 0
+//  for (let i = 0; i < populacao.length; i++) {
+//    faixaAcumulada += populacao[i].probabilidade
+//    faixasProbabilidade.push(faixaAcumulada)
+//  }
+//
+//  const sorteio = Math.random()
+//  for (let i = 0; i < faixasProbabilidade.length; i++) {
+//    if (sorteio <= faixasProbabilidade[i]) {
+//      return populacao[i]
+//    }
+//  }
+//}
 
 //Resultados
 const populacao = novaPopulacao(nInd, cromLimite)
-const populationFinal = convertToBin(populacao)
-const populacaoFitness = fitness(populacao)
-const selecionado = metodoDaRoleta(populacaoFitness)
 console.log(populacao)
-//console.log(convertToBin(populacao))
-//console.log(decodeBinaries(populationFinal))
-//console.log(fitness(ordenacao))
-//console.log(`selecionado: ${JSON.stringify(selecionado)}`)
+console.log(normalize(populacao))
